f0fd9d71097fb07ab2eb18c6107e0c3b576a0a70
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-16 22:21:51 +0300

    Update build-tokens.mjs

--- build-tokens.mjs
+++ build-tokens.mjs
@@ -3,7 +3,7 @@ import { registerTransforms } from '@tokens-studio/sd-transforms';
 
 // sd-transforms, 2nd parameter for options can be added
 // See docs: https://github.com/tokens-studio/sd-transforms
-register(StyleDictionary, {
+registerTransforms(StyleDictionary, {
   expand: {
     composition: false,
     typography: false,
