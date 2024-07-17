34f32f2895f934f280c7f852db2cbb8331d5ef68
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-18 00:45:36 +0300

    Update build-tokens.mjs

--- build-tokens.mjs
+++ build-tokens.mjs
@@ -3,30 +3,29 @@ import { register } from '@tokens-studio/sd-transforms';
 
 // sd-transforms, 2nd parameter for options can be added
 // See docs: https://github.com/tokens-studio/sd-transforms
-register(StyleDictionary);
+register(StyleDictionary, {
+  expand: {
+    composition: false,
+    typography: false,
+    border: false,
+    shadow: false,
+  },
+  excludeParentKeys: true,
+});
 
 const sd = new StyleDictionary({
-  source: ['studio.json'],
+  source: ['tokens.json'],
   preprocessors: ['tokens-studio'],
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
-      prefix: 'sd',
+      transforms: ['name/kebab'],
+      prefix: 'rd',
       buildPath: 'build/css/',
       files: [
         {
           destination: '_variables.css',
           format: 'css/variables',
-        },
-      ],
-    },
-    js: {
-      transformGroup: 'tokens-studio',
-      buildPath: 'build/js/',
-      files: [
-        {
-          destination: 'variables.js',
-          format: 'javascript/es6',
         },
       ],
     },
