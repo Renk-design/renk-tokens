3f3174c8942af4c743e9eb1f68aec5655c699736
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-18 00:39:46 +0300

    Add files via upload

---
+++ build-tokens.mjs
@@ -0,0 +1,37 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+
+// sd-transforms, 2nd parameter for options can be added
+// See docs: https://github.com/tokens-studio/sd-transforms
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
+  source: ['studio.json'],
+  preprocessors: ['tokens-studio'],
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio',
+      prefix: 'sd',
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: '_variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+    js: {
+      transformGroup: 'tokens-studio',
+      buildPath: 'build/js/',
+      files: [
+        {
+          destination: 'variables.js',
+          format: 'javascript/es6',
+        },
+      ],
+    },
+  },
+});
+// optionally, cleanup files first..
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
